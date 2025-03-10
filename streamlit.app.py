10827c63585a4721d774f67cdd4cb2085920f1b0
Novelia Arum <noveliaarum@gmail.com>
2025-03-10 11:11:38 +0700

    first commit

---
+++ streamlit.app.py
@@ -0,0 +1,13 @@
+import streamlit as st
+import cv2
+from PIL import Image, ImageEnhance
+import numpy as np
+
+def main():
+    """Face Detection App"""
+    st.title("Face Detection App")
+    st.text("Build with streamlit and Open CV"
+            )
+    
+if __name__ == '__Main__':
+    main()
